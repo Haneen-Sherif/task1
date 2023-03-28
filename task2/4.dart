2b16587da9243388ff6bc384ff12f8e1d45773fa
Haneen-Sherif <104598574+Haneen-Sherif@users.noreply.github.com>
2023-03-28 21:04:41 +0200

    Update 4.dart

--- task2/4.dart
+++ task2/4.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  int? num = int.parse(stdin.readLineSync()!);
-  var list = [];
-  list.add(num);
-  if (list.length > 0) {
-    print("true");
+ int? num = int.parse(stdin.readLineSync()!);
+  var list = [1, 2, 3, 4, 5];
+  if (num > list.length) {
+    print("greater than the length of the list");
+  } else if (num < list.length) {
+    print("less than the length of the list");
   } else {
-    print("false");
+    print("equals the length of the list");
   }
 }
